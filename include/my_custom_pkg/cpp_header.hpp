1e8519dfe7542df679d7a666615d3aa699a10559
Than Zaw Toe <thanzawtoe.tzt@gmail.com>
2023-08-10 00:02:08 +0630

    Create custom pkg with C++ and Python node together

---
+++ include/my_custom_pkg/cpp_header.hpp
@@ -0,0 +1,11 @@
+#include "rclcpp/rclcpp.hpp"
+
+class MyCustomNode : public rclcpp::Node
+{
+    public:
+        MyCustomNode(): Node("my_node") {
+            RCLCPP_INFO(this->get_logger(), "TEST Cpp Node");
+        }
+        
+    private:
+};
